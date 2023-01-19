d371c4aeeff6386553d16df9f441445e4fcc597e
Caramel-02 <cz123@student.ugal.ro>
2023-01-19 15:12:29 +0200

    proiect pus

---
+++ src/filter/CafeaCapsuleFilter.java
@@ -0,0 +1,31 @@
+package filter;
+
+import java.util.ArrayList;
+import model.CafeaCapsule;
+
+
+public class CafeaCapsuleFilter {
+    
+    public static ArrayList<CafeaCapsule>filtreazaDupaBrand(ArrayList<CafeaCapsule>listaInitialaCafeaCapsule,String brand)
+    //lista de tip cafeacapsule si contine ca parametri lista initiala de cafeaCapsule si brandul dupa care se filtreaza
+    {
+        ArrayList<CafeaCapsule>listaCafeaCapsule= new ArrayList<>();
+        for(CafeaCapsule cafeaCapsule: listaInitialaCafeaCapsule)
+            if(cafeaCapsule.getBrand().equals(brand))
+            listaCafeaCapsule.add(cafeaCapsule);
+        return listaCafeaCapsule;
+    }
+
+
+
+public static ArrayList<CafeaCapsule>filtreazaDupaCofeina(ArrayList<CafeaCapsule>listaInitialaCafeaCapsule,String cofeina)
+//lista de tip cafeacapsule si contie ca parametri lista initiala de cafeaCapsule si cofeina dupa care se filtreaza
+{
+    ArrayList<CafeaCapsule>listaCafeaCapsule= new ArrayList<>();
+    for(CafeaCapsule cafeaCapsule: listaInitialaCafeaCapsule)
+        if(cafeaCapsule.getBrand().equals(cofeina))
+        listaCafeaCapsule.add(cafeaCapsule);
+    return listaCafeaCapsule;
+}
+
+}
